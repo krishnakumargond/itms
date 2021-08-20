ae0c8ff59b23fb8de0fd416d0dd6d341db8bab59
vinay1028 <vinaysingh1028@gmail.com>
2021-08-20 00:03:53 -0700

    updated by vinay

--- itms/src/app/master/user/components/user-form/user-form.component.ts
+++ itms/src/app/master/user/components/user-form/user-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-form',
@@ -8,11 +9,35 @@ import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 export class UserFormComponent implements OnInit, AfterViewInit {
   @ViewChild('dropdown4') list;
   @ViewChild('dropdown5') list1;
+  formValue:FormGroup;
   myList:any;
   myList1:any;
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.formValue= this.formBuilder.group({
+      userName: new FormControl('',[
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(255)
+      ]),
+      employee: new FormControl('',),
+      role: new FormControl(''),
+      mobile:new FormControl(null,[
+        Validators.required
+      ]),
+      password:new FormControl(null,[
+        Validators.required,
+        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*]).{8,}$')
+      ]),
+      loginID:new FormControl('',[
+        Validators.required
+      ]),
+      status:new FormControl(false,[
+        Validators.required
+      ])
+
+    })
   }
 
   ngAfterViewInit(){
--- itms/src/app/master/zone/components/zone-form/zone-form.component.ts
+++ itms/src/app/master/zone/components/zone-form/zone-form.component.ts
@@ -1,4 +1,5 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-zone-form',
@@ -8,10 +9,28 @@ import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 export class ZoneFormComponent implements OnInit, AfterViewInit {
 @ViewChild('drop') list;
 zoneList:any;
-  constructor() { }
+formValue:FormGroup;
+
+  constructor(private formBuilder:FormBuilder) { }
 
   ngOnInit(): void {
+    this.formValue = this.formBuilder.group({
+      selectCircle: new FormControl(''),
+      zoneName:new FormControl('',[
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(15)
+      ]),
+      zoneShortName:new FormControl('',[
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(5)
+      ]),
+      status:new FormControl(false)
+    })
   }
+
+  
   ngAfterViewInit(){
     this.zoneList = $(this.list.nativeElement);
     this.zoneList.select2({
